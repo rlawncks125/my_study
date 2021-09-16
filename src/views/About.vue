099140381a93f384fa33c3cf289cd0f2ba6ee899
김주찬 <rlawncks125@naver.com>
2021-09-16 13:52:38 +0900

    traivs 테스트

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="about">
-    <h1>
-      This is an about page
-      asdsaaaaaaaaaaaaaaaaaaaaaaaaaaaawwwwwwwwwwwwwwwwwwwwwwwwwwxxxxxxxxxxxxxxxxxxxxxx
-    </h1>
+    <h1>travis 배포 테스트 합니다.</h1>
   </div>
 </template>
