19375ad1fe6f93cce4594505011375177f6dae48
rlawncks125 <rlawncks125@users.noreply.github.com>
2022-05-14 13:08:20 +0000

    deploy: 2cc7523ae55e4c7bbf5dbd8c12f99b475c3ee28f

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf1ef99b9c62fff0429301e93f90a666.js"
+  "/precache-manifest.498342d4723490ede07ce1d90dd14816.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "my_study"});
